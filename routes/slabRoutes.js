c382830b9ed6c3ee7f028b07be67ffaa73ed6b36
Praveen Nitharwal <praveennitharwal55@gmail.com>
2025-12-04 03:30:14 +0530

    Fix slab routes import + middleware

--- routes/slabRoutes.js
+++ routes/slabRoutes.js
@@ -2,18 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
+
 const {
   createSlab,
   getSlabs,
   getSingleSlab,
   updateSlab,
-  deleteSlab
+  deleteSlab,
 } = require("../controllers/slabController");
 
+// CREATE
 router.post("/", authMiddleware, createSlab);
+
+// GET ALL
 router.get("/", authMiddleware, getSlabs);
+
+// GET SINGLE
 router.get("/:id", authMiddleware, getSingleSlab);
+
+// UPDATE
 router.put("/:id", authMiddleware, updateSlab);
+
+// DELETE
 router.delete("/:id", authMiddleware, deleteSlab);
 
 module.exports = router;
